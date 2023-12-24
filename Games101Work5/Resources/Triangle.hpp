c8efd1a037a702944551ad71090c56ca29b69b67
ShenLu-jd <2311278320@qq.com>
2023-12-24 15:21:04 +0800

    #feat 第五次作业添加个别注释

--- Games101Work5/Resources/Triangle.hpp
+++ Games101Work5/Resources/Triangle.hpp
@@ -11,6 +11,8 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
+
+    // 关于下面的射线与三角形相交的计算可查看该博客 https://www.cnblogs.com/graphics/archive/2010/08/09/1795348.html
     Vector3f E1 = v1 - v0, E2 = v2 - v0;
     Vector3f S = orig - v0;
     Vector3f S1 = crossProduct(dir, E2), S2 = crossProduct(S, E1);
