ddd69ff17092f3101a151f8004f23af87e5bf0e4
ShenLu-jd <2311278320@qq.com>
2024-01-07 20:57:48 +0800

    #bugfix 第六次作业问题：相交判定条件修改

--- Games101Work6/Resources/Triangle.hpp
+++ Games101Work6/Resources/Triangle.hpp
@@ -232,6 +232,8 @@ inline Intersection Triangle::getIntersection(Ray ray)
     t_tmp = dotProduct(e2, qvec) * det_inv;
 
     // TODO find ray triangle intersection
+    if (t_tmp < 0)
+        return inter;
 
     inter.happened = true;
     inter.coords = ray(t_tmp);
